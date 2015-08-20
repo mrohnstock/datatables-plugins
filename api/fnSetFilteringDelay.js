784b7c1141b34888b21cfc3ee2aad42ac693331e
Mathias Rohnstock <m.rohnstock@googlemail.com>
2015-08-20 12:26:23 +0200

    DataTables-Plugins v1.10.8

--- api/fnSetFilteringDelay.js
+++ api/fnSetFilteringDelay.js
@@ -6,6 +6,10 @@
  * where you wouldn't typically want an Ajax request to be made with every key
  * press the user makes when searching the table.
  *
+ * Please note that this plug-in has been deprecated and the `dt-init
+ * searchDelay` option in DataTables 1.10 should now be used. This plug-in will
+ * not operate with v1.10+.
+ *
  *  @name fnSetFilteringDelay
  *  @summary Add a key debouce delay to the global filtering input of a table
  *  @author [Zygimantas Berziunas](http://www.zygimantas.com/), 
@@ -27,13 +31,11 @@ jQuery.fn.dataTableExt.oApi.fnSetFilteringDelay = function ( oSettings, iDelay )
 	this.each( function ( i ) {
 		$.fn.dataTableExt.iApiIndex = i;
 		var
-			$this = this,
 			oTimerId = null,
 			sPreviousSearch = null,
 			anControl = $( 'input', _that.fnSettings().aanFeatures.f );
 
-			anControl.unbind( 'keyup search input' ).bind( 'keyup search input', function() {
-			var $$this = $this;
+		anControl.unbind( 'keyup search input' ).bind( 'keyup search input', function() {
 
 			if (sPreviousSearch === null || sPreviousSearch != anControl.val()) {
 				window.clearTimeout(oTimerId);
